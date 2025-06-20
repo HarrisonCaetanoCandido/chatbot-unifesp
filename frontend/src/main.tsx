e03ba405766b69b13bfac40e071c6e842bda8821
HarrisonCaetanoCandido <caetanocandido.h@gmail.com>
2025-06-20 19:29:52 -0300

    chore: just removing a comment

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,7 @@
-// define nosso root directory como sendo src/
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
