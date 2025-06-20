dc5551394af0526cbceb3940835c9501407e0129
HarrisonCaetanoCandido <caetanocandido.h@gmail.com>
2025-06-20 19:32:53 -0300

    feat: new routes such as termos de uso and politica de privacidade added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,20 @@
-import './App.css'
-import ConvoContainer from './components/conversation/ConvoContainer'
-import EmptyConvoContainer from './components/conversation/EmptyConvoContainer'
-import CustomFooter from './components/footer/CustomFooter'
-import CustomHeader from './components/header/CustomHeader'
+import { BrowserRouter, Routes, Route } from 'react-router';
+import Chat from './pages/chat/Chat';
+import TermsOfUse from './pages/terms-of-use/TermsOfUse';
+import PrivacyPolicy from './pages/privacy-policy/PrivacyPolicy';
+import { Toaster } from 'react-hot-toast';
 
-function App() {
-  let startedConvo: Boolean = false;
-
-  return (
-    <div className='flex flex-col h-screen overflow-hidden'>
-      <CustomHeader />
-      {startedConvo ? (<ConvoContainer />) : (<EmptyConvoContainer />)}
-      <CustomFooter />
-    </div>
-  )
+export default function App() {
+    return (
+        <>
+            <Toaster position="bottom-right" reverseOrder={false} />
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Chat />} />
+                    <Route path="/termos-de-uso" element={<TermsOfUse />} />
+                    <Route path="/politica-de-privacidade" element={<PrivacyPolicy />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
-
-export default App
