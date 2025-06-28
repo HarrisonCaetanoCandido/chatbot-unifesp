4fbce335c613e26edde5c7519fb0707dc1716034
HarrisonCaetanoCandido <caetanocandido.h@gmail.com>
2025-06-28 17:16:53 -0300

    feat(chat): sessionStorage added to keep current chat alive

--- frontend/src/store/index.tsx
+++ frontend/src/store/index.tsx
@@ -1,17 +1,37 @@
 import { create } from 'zustand';
 import { fromIntroToChatSlice } from './slices/fromIntroToChatSlice';
 import { conversationSlice, type Conversation } from './slices/conversationSlice';
+import { persist } from 'zustand/middleware';
 
 type AppState = {
-    chatInitialized: Boolean;
-    setChatInitialized: (value: Boolean) => void;
-    selectedConvoId?: string;
-    setConvoId: (convoId: string) => void;
-    convo: Conversation[];
-    setConvo: (convo: Conversation) => void;
-}
+  chatInitialized: boolean;
+  setChatInitialized: (value: boolean) => void;
+  selectedConvoId?: string;
+  setConvoId: (convoId: string) => void;
+  convo: Conversation[];
+  setConvo: (convo: Conversation) => void;
+};
 
-export const useStore = create<AppState>((set) => ({
-    ...fromIntroToChatSlice(set),
-    ...conversationSlice(set)
-}));
+export const useStore = create<AppState>()(
+  persist(
+    (set) => ({
+      ...fromIntroToChatSlice(set),
+      ...conversationSlice(set),
+    }),
+    {
+      name: 'convo-storage',
+      storage: {
+        getItem: (name) => {
+          const item = sessionStorage.getItem(name);
+          return item ? JSON.parse(item) : null;
+        },
+        setItem: (name, value) => {
+          sessionStorage.setItem(name, JSON.stringify(value));
+        },
+        removeItem: (name) => {
+          sessionStorage.removeItem(name);
+        },
+      },
+    }
+  )
+);
